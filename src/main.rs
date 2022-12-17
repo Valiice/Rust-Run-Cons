a8b5ef3f802aca87dbe1c983eb9410950b7c520c
Valiice <valentin.claes@student.howest.be>
2022-12-17 12:13:56 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use std::process::Command;
+
+fn main() {
+    let directories = fs::read_dir(".").unwrap();
+
+    for dir in directories {
+        let dir = dir.unwrap();
+        let dir_name = dir.file_name();
+        if dir_name.to_str().unwrap().ends_with(".Cons") {
+            Command::new("dotnet")
+                .arg("run")
+                .current_dir(dir.path())
+                .spawn()
+                .expect("failed to execute process");
+        }
+    }
+    println!("Wait for all the clients to run.");
+    let mut line = String::new();
+    let _input = std::io::stdin().read_line(&mut line).expect("Failed to read line");
+}
